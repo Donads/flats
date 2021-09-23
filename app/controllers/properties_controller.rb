bea2ea5e9f7a12c9b0483279f212e37e158f8de1
Yan Donadi <yan.donadi@gmail.com>
2021-09-23 15:22:40 -0300

    Implement create and show with tests

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,19 @@
 class PropertiesController < ApplicationController
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(params.require(:property).permit(:title, :description, :rooms, :parking_slot, :bathrooms,
+                                                              :pets, :daily_rate))
+
+    if @property.save
+      redirect_to @property
+    else
+      redirect_to new_property_path
+    end
+  end
+
   def show
     @property = Property.find(params[:id])
   end
